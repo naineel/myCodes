f39d54fb3965f0c9965e9b25004329361e33e057
naineel <shah.nai@husky.neu.edu>
2015-04-30 00:03:05 -0400

    xml xlst

--- CS5200 XML XSLT assignment/Solution/src/cs5200/jpa/orm/entity/Tower.java
+++ CS5200 XML XSLT assignment/Solution/src/cs5200/jpa/orm/entity/Tower.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.xml.bind.annotation.XmlAccessType;
@@ -32,7 +34,8 @@ public class Tower {
 	private double height;
 	@XmlAttribute
 	private Integer sides;
-	@XmlAttribute
+	@ManyToOne
+	@JoinColumn(name="siteId")
 	@XmlTransient
 	private Site site;
 	@OneToMany(mappedBy="tower", cascade=CascadeType.ALL, orphanRemoval=true)
